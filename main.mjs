0bb03eb4d7a0ae8a7fb39b8f72db5897d256a9da
Ed Ezekiel <ed@ubiquisoft.com>
2020-03-19 16:35:46 -0500

    Integrate readline prompt

--- main.mjs
+++ main.mjs
@@ -1,10 +1,25 @@
 import { serverConnections } from './modules/serverConnections.mjs';
 import { parsedConfigFile } from './modules/parsedConfigFile.mjs';
+import readline from 'readline';
+import process from 'process';
 
 const main = () => {
     const connections = serverConnections(parsedConfigFile);
     console.log(connections);
+
+    getPassword();
     return connections;
 }
 
+const getPassword = () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+    rl.question("Enter your FedEx password: ", (password) => {
+        console.log(`${password}`)
+        rl.close();
+    })
+}
 main();
