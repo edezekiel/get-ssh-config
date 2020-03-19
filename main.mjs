928ac976fa47ca40aa5468379af5a7e34d212c1e
Ed Ezekiel <ed@ubiquisoft.com>
2020-03-19 15:30:41 -0500

    Separate parsedConfigFile out into a separate module

--- main.mjs
+++ main.mjs
@@ -1,27 +1,10 @@
 import { serverConnections } from './modules/serverConnections.mjs';
-import fs from 'fs';
-import SSHConfig from 'ssh-config';
-import process from 'process';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { parsedConfigFile } from './modules/parsedConfigFile.mjs';
 
-const sshConfigFile = () => {
-    // __dirname is not defined in Node ES Modules
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-
-    process.chdir(process.env.HOME);
-
-    // "~/.ssh/config" path doesn't work. Have to change directories instead.
-    const config = fs.readFileSync('.ssh/config', {encoding: 'utf8'}, (err, data) => {
-        if (err) throw err;
-        return data;
-    }).toString();
-
-    process.chdir(__dirname);
-    return config;
+const main = () => {
+    const connections = serverConnections(parsedConfigFile);
+    console.log(connections);
+    return connections;
 }
 
-const parsedConfigFile = SSHConfig.parse(sshConfigFile());
-
-console.log(serverConnections(parsedConfigFile));
+main();
